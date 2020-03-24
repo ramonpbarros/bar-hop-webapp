6a55a0e4cc5bab97f49fb1c340fd13567f00864f
Sorrab <ramonpbarros@gmail.com>
2020-03-24 16:07:10 -0700

    added navBar

---
+++ assets/js/foundation.js
@@ -0,0 +1,5 @@
+$('#element').foundation('down', $target);
+$('#element').foundation('hideAll');
+$('#element').foundation('showAll');
+$('#element').foundation('toggle', $target);
+var elem = new Foundation.AccordionMenu(element, options);
